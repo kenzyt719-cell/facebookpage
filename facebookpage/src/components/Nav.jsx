d28e19f4887f4de018c6b2833619ca0cacbd789d
Kenzy Tarek <kenzy@Kenzys-MacBook-Pro.local>
2025-10-23 12:49:16 +0300

    navbar2

--- facebookpage/src/components/Nav.jsx
+++ facebookpage/src/components/Nav.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import "./Nav.css"
+import Imgface from '../assets/logoface.png';
+import Imghome from '../assets/homenav.png';
+
 
 const Nav = () => {
     return ( 
       <div className='nav'>
         
+
         
+        <div className='allhomes'>
+        <img className='face' src={Imgface} alt="face" />
+
+        <img className='home' src={Imghome} alt="home" />
+        <img className='home' src={Imghome} alt="home" />
+        <img className='home' src={Imghome} alt="home" />
+        <img className='home' src={Imghome} alt="home" />
+
+        </div>
+      
+        
+
 
       </div>
 
